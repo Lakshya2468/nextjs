8867ebb8c6547051d6a1810d305a5f0ca26eec53
Lakshya2468 <lakshyakhandelwal7769@gmail.com>
2023-09-15 15:21:37 +0530

    complete

--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -1,33 +1,97 @@
-
-import { useEffect ,useState} from "react";
-
-
+import React, { useEffect, useState } from "react";
 
 export function Header() {
-    const [owner,setowner]=useState([]);
-    useEffect(() => {
-        async function fetchData() {
-          try {
-            const response = await fetch("http://localhost:3001/owner");
-           
-            const result = await response.json();
-            console.log(result)
-            setowner(result)
-        
-          } catch (error) {
-            console.error("Error fetching data:", error);
-          }
-        }
-    
-        fetchData();
-      }, []);
-   return (
+  const [owners, setOwners] = useState([]);
+  const [newOwnerName, setNewOwnerName] = useState("");
+  const [newOwnerGender, setNewOwnerGender] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:3001/owner");
+        const result = await response.json();
+        console.log(result);
+        setOwners(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+     
+      await fetch(`http://localhost:3001/owner/${id}`, {
+        method: "DELETE",
+      });
+
+      const updatedOwners = owners.filter((item) => item.id !== id);
+      setOwners(updatedOwners);
+    } catch (error) {
+      console.error("Error deleting owner:", error);
+    }
+  };
+
+  const handleCreate = async () => {
+    try {
+   
+      const response = await fetch("http://localhost:3001/owner", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          o_name: newOwnerName,
+          gender: newOwnerGender,
+        }),
+      });
+      if (response.ok) {
+   
+        const updatedResponse = await fetch("http://localhost:3001/owner");
+        const updatedResult = await updatedResponse.json();
+        setOwners(updatedResult);
+      } else {
+        console.error("Error creating owner:", response.status);
+      }
+      setNewOwnerName("");
+      setNewOwnerGender("");
+    } catch (error) {
+      console.error("Error creating owner:", error);
+    }
+  };
+
+  return (
     <>
       <h1 className="p-6 text-3xl bg-lime-500 text-white">Owner list---</h1>
-      {owner.map((item) => (
-        <h3 className="font-bold m-4" key={item.id}>{item.o_name}</h3>
-      ))}
+      <input className="mr-3" placeholder="Enter Owner Name" type="text" value={newOwnerName} onChange={(e) => setNewOwnerName(e.target.value)} />
+      <input className="mr-3" placeholder="Gender" type="text" value={newOwnerGender} onChange={(e) => setNewOwnerGender(e.target.value)} />
+      <button onClick={handleCreate}>Create</button>
+
+      <table className="m-5 ">
+        <thead>
+          <tr >
+            <th>Owner name</th>
+            <th>Gender</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {owners.map((item) => (
+            <tr key={item.id}>
+              <td>{item.o_name}</td>
+              <td>{item.gender}</td>
+              <td>
+                <button onClick={() => handleDelete(item.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      
     </>
   );
 }
-  export default Header;
+
+export default Header;
--- src/pages/components/vehicle.tsx
+++ src/pages/components/vehicle.tsx
@@ -4,6 +4,9 @@ import { useEffect ,useState} from "react";
 
 export function Vehicle() {
     const [vehicle,setvehicle]=useState([]);
+    const [newvehiclename, setNewvehicleName] = useState("");
+    const [newvehicletype, setNewvehicletype] = useState("");
+    const [newvehicleid, setNewvehicleid] = useState("");
     useEffect(() => {
         async function fetchData() {
           try {
@@ -20,17 +23,84 @@ export function Vehicle() {
     
         fetchData();
       }, []);
+
+      const handleDelete = async (id) => {
+        try {
+         
+          await fetch(`http://localhost:3001/vehicle/${id}`, {
+            method: "DELETE",
+          });
+    
+          const updatedVehicle = vehicle.filter((item) => item.id !== id);
+          setvehicle(updatedVehicle);
+        } catch (error) {
+          console.error("Error deleting vehicle:", error);
+        }
+      };
+      
+  const handleCreate = async () => {
+    try {
+   
+      const response = await fetch("http://localhost:3001/vehicle", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+       
+        body: JSON.stringify({
+          v_name: newvehiclename,
+          type: newvehicletype,
+          id:newvehicleid
+        }),
+       
+        
+      });
+      if (response.ok) {
+   
+        const updatedResponse = await fetch("http://localhost:3001/vehicle");
+        const updatedResult = await updatedResponse.json();
+        setvehicle(updatedResult);
+      } else {
+        console.error("Error creating owner:", response.status);
+      }
+      setNewvehicleName("");
+      setNewvehicletype("");
+      setNewvehicleid("");
+    } catch (error) {
+      console.error("Error creating owner:", error);
+    }
+  };
    return (
     <>
-      <h1 className="p-6 text-2xl bg-lime-500 text-white">Vehicle list---</h1>
-     
-        <ul>
-        {vehicle.map((item) => (
-            <li className="font-bold m-4" key={item.id}> {item.v_name}</li>
-            ))}
-        </ul>
-    
-    
+    <h1 className="p-6 text-3xl bg-lime-500 text-white">Vehicle list---</h1>
+      <input className="mr-3" placeholder="Enter Vehicle name" type="text" value={newvehiclename} onChange={(e) => setNewvehicleName(e.target.value)} />
+      <input className="mr-3" placeholder="Type" type="text" value={newvehicletype} onChange={(e) => setNewvehicletype(e.target.value)} />
+      <input className="mr-3" type="number" placeholder="Owner id" value={newvehicleid} onChange={(e) => setNewvehicleid((e.target.value))} />
+
+      <button onClick={handleCreate}>Create</button>
+    <table className="m-5 ">
+        <thead>
+          <tr >
+            <th>Vehicle name</th>
+            <th>Type</th>
+            <th>ownerid</th>
+            
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {vehicle.map((item) => (
+            <tr key={item.id}>
+              <td>{item.v_name}</td>
+              <td>{item.type}</td>
+              <td>{item.ownerid}</td>
+              <td>
+                <button onClick={() => handleDelete(item.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
